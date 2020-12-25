e5f6d0c2cf92980ebac3e2abae99628b4ddecdb4
omaddam <omaddam@gmail.com>
2020-12-25 01:17:32 -0700

    Referenced the mesh filter and mesh renderer on start.

--- Assets/VoronoiDiagram/Scripts/DiagramRegion.cs
+++ Assets/VoronoiDiagram/Scripts/DiagramRegion.cs
@@ -9,13 +9,28 @@ namespace VoronoiDiagram
 
         #region Initialization
 
-
+        /// <summary>
+        /// Executes once on start.
+        /// </summary>
+        private void Awake()
+        {
+            MeshRenderer = GetComponent<MeshRenderer>();
+            MeshFilter = GetComponent<MeshFilter>();
+        }
 
         #endregion
 
         #region Fields/Properties
 
+        /// <summary>
+        /// References the mesh renderer of this object. Used to set the color of the mesh.
+        /// </summary>
+        private MeshRenderer MeshRenderer;
 
+        /// <summary>
+        /// References the mesh filter of this object. Used to set the mesh.
+        /// </summary>
+        private MeshFilter MeshFilter;
 
         #endregion
 
