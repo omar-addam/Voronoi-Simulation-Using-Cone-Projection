781f924f82a2de908d788c33338f65917daf6ec5
omaddam <omaddam@gmail.com>
2020-12-25 01:10:24 -0700

    Created an empty diagram region and attached to it an emtpy script.

---
+++ Assets/VoronoiDiagram/Scripts/DiagramRegion.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoronoiDiagram
+{
+    public class DiagramRegion : MonoBehaviour
+    {
+
+        #region Initialization
+
+
+
+        #endregion
+
+        #region Fields/Properties
+
+
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+
+    }
+}
