37f51506671db19d14e7c93a44aa44ce0ef3e2af
omaddam <omaddam@gmail.com>
2020-12-25 14:48:00 -0700

    References the collider of the diagram on awake.

--- Assets/VoronoiDiagram/Scripts/DiagramManager.cs
+++ Assets/VoronoiDiagram/Scripts/DiagramManager.cs
@@ -7,5 +7,26 @@ namespace VoronoiDiagram
     public class DiagramManager : MonoBehaviour
     {
 
+        #region Initialization
+
+        /// <summary>
+        /// Executes once on start.
+        /// </summary>
+        private void Awake()
+        {
+            Collider = GetComponent<Collider2D>();
+        }
+
+        #endregion
+
+        #region Fields/Properties
+
+        /// <summary>
+        /// References the collider of the diagram that will be used to determine its bounds.
+        /// </summary>
+        private Collider2D Collider;
+
+        #endregion
+
     }
 }
