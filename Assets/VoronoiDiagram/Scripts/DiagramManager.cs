d2bf007413b609c2d702000c52770fae53d0820a
omaddam <omaddam@gmail.com>
2020-12-25 14:55:37 -0700

    Referenced the region prefab used as a template for creating regions.

--- Assets/VoronoiDiagram/Scripts/DiagramManager.cs
+++ Assets/VoronoiDiagram/Scripts/DiagramManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VoronoiDiagram.DataStructure;
 
 namespace VoronoiDiagram
 {
@@ -21,7 +22,8 @@ namespace VoronoiDiagram
         /// Initializes the diagram.
         /// </summary>
         /// <param name="dimensions">The width and length of the diagram.</param>
-        public void Initialize(Vector2 dimensions)
+        /// <param name="seeds">The center of each region to be created.</param>
+        public void Initialize(Vector2 dimensions, List<Seed> seeds)
         {
             // Set the width and length of the diagram
             transform.localScale = new Vector3(dimensions.x, dimensions.y, 0);
@@ -35,6 +37,24 @@ namespace VoronoiDiagram
         /// References the collider of the diagram that will be used to determine its bounds.
         /// </summary>
         private Collider2D Collider;
+
+        /// <summary>
+        /// References the template prefab used when initializing the regions.
+        /// </summary>
+        [SerializeField]
+        private GameObject RegionTemplate;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Creates a single region for each provided seed.
+        /// </summary>
+        private void InitializeRegions(List<Seed> seeds)
+        {
+
+        }
 
         #endregion
 
