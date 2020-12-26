f7dc172f7fd8e0faf0bd80f39657130f8b4769bc
omaddam <omaddam@gmail.com>
2020-12-26 00:36:33 -0700

    Referenced the diagram.

--- Assets/App/Scripts/MainSceneManager.cs
+++ Assets/App/Scripts/MainSceneManager.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 public class MainSceneManager : MonoBehaviour
 {
 
+    #region Initialization
 
+
+
+    #endregion
+
+    #region Fields/Properties
+
+    /// <summary>
+    /// References the voronoi diagram in the scene.
+    /// </summary>
+    [SerializeField]
+    private VoronoiDiagram.DiagramManager Diagram;
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
 
 }
