35cfe5b1ac02df41a2e38394bbb737e39208dcdc
omaddam <omaddam@gmail.com>
2020-12-26 01:21:11 -0700

    Added a 2 regions sample.

--- Assets/App/Scripts/MainSceneManager.cs
+++ Assets/App/Scripts/MainSceneManager.cs
@@ -42,8 +42,8 @@ public class MainSceneManager : MonoBehaviour
         // Display version
         VersionText.text = string.Format("Version: {0}", Application.version);
 
-        // Starts the demo with a 3 regions sample.
-        Generate3RegionsSample();
+        // Starts the demo with a 2 regions sample.
+        Generate2RegionsSample();
     }
 
     #endregion
@@ -67,6 +67,18 @@ public class MainSceneManager : MonoBehaviour
     #region Methods
 
     /// <summary>
+    /// Generates 2 regions sample.
+    /// </summary>
+    public void Generate2RegionsSample()
+    {
+        Diagram.Initialize(new Vector2(DIARGRAM_WIDTH, DIARGRAM_HEIGHT), new List<Seed>()
+        {
+            new Seed(Guid.NewGuid(), CIRLCE_SEGMENTS_COUNT, 1.5f, 1.5f, Color.red),
+            new Seed(Guid.NewGuid(), CIRLCE_SEGMENTS_COUNT, -1.5f, -1.5f, Color.blue),
+        }, SIMULATION_SPEED);
+    }
+
+    /// <summary>
     /// Generates 3 regions sample.
     /// </summary>
     public void Generate3RegionsSample()
@@ -87,7 +99,7 @@ public class MainSceneManager : MonoBehaviour
         Diagram.Initialize(new Vector2(DIARGRAM_WIDTH, DIARGRAM_HEIGHT), new List<Seed>()
         {
             new Seed(Guid.NewGuid(), CIRLCE_SEGMENTS_COUNT, 0.5f, 0.5f, Color.red),
-            new Seed(Guid.NewGuid(), CIRLCE_SEGMENTS_COUNT, 0f, 0f, Color.cyan),
+            new Seed(Guid.NewGuid(), CIRLCE_SEGMENTS_COUNT, 2f, 0f, Color.cyan),
             new Seed(Guid.NewGuid(), CIRLCE_SEGMENTS_COUNT, -1.5f, -1.5f, Color.blue),
             new Seed(Guid.NewGuid(), CIRLCE_SEGMENTS_COUNT, -0.5f, -1.5f, Color.green)
         }, SIMULATION_SPEED);
